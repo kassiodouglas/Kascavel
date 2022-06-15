92d7e712b5433424fbba198bdeb402d88786d932
cassio.souza <cassio.souza@grupotel.com.br>
2022-06-15 20:06:14 -0300

    Arquivos de configurações

---
+++ src/app/config/__init__.py
@@ -0,0 +1,2 @@
+from src.app.config.app import *
+from src.app.config.console import *
---
+++ src/app/config/console.py
@@ -0,0 +1,11 @@
+from src.core import Environment
+
+console = {
+    'commands_admin':[
+        'server',           #Inicia o servidor
+        'make',             #Criação
+        'migrate',          #Migração de banco de dados
+        'help',             #Ajuda
+        'list',             #Listagem de itens 
+    ]  
+}
